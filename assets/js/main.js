1b1a7c8cf0e04f4a2b02de0f5dddfc54390772b2
Mohammadostadi <mohammad.asram2000@gmail.com>
2024-03-06 08:31:29 +0330

    ecommerce-product

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,11 @@ const plusButton = document.getElementById('plus');
 const inputField = document.getElementById('input');
 
 minusButton.addEventListener('click', event => {
-    event.preventDefault();
-    const currentValue = Number(inputField.value) || 0;
-    inputField.value = currentValue - 1;
+    if (inputField.value > 0 ){
+        event.preventDefault();
+        const currentValue = Number(inputField.value) || 0;
+        inputField.value = currentValue - 1;
+    }
 });
 
 plusButton.addEventListener('click', event => {
