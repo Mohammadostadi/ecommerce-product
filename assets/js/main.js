bc9b43fecaca3906f5716f5ada3c7600d644fc1b
Mohammadostadi <mohammad.asram2000@gmail.com>
2024-03-08 11:40:10 +0330

    editing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+'use strict'
 const minusButton = document.getElementById('minus');
 const plusButton = document.getElementById('plus');
 const inputField = document.getElementById('input');
@@ -48,3 +49,75 @@ function closePopUp(){
     show = true
     popUps.classList.remove("show")
 }
+
+var ImageProduct1 = document.getElementById('image-product-1')
+var ImageProduct2 = document.getElementById('image-product-2')
+var ImageProduct3 = document.getElementById('image-product-3')
+var ImageProduct4 = document.getElementById('image-product-4')
+var ImageProductThumbnail1 = document.getElementById('image-product-1-thumbnail')
+var ImageProductThumbnail2 = document.getElementById('image-product-2-thumbnail')
+var ImageProductThumbnail3 = document.getElementById('image-product-3-thumbnail')
+var ImageProductThumbnail4 = document.getElementById('image-product-4-thumbnail')
+
+function showImageProduct1(){
+    ImageProduct1.classList.add("active-image")
+    ImageProduct2.classList.remove("active-image")
+    ImageProduct3.classList.remove("active-image")
+    ImageProduct4.classList.remove("active-image")
+    ImageProduct1.classList.remove("non-active-image")
+    ImageProduct2.classList.add("non-active-image")
+    ImageProduct3.classList.add("non-active-image")
+    ImageProduct4.classList.add("non-active-image")
+    ImageProductThumbnail1.classList.add('active-item')
+    ImageProductThumbnail2.classList.remove('active-item')
+    ImageProductThumbnail3.classList.remove('active-item')
+    ImageProductThumbnail4.classList.remove('active-item')
+}
+
+function showImageProduct2(name){
+        ImageProduct2.classList.add("active-image")
+        ImageProduct1.classList.remove("active-image")
+        ImageProduct3.classList.remove("active-image")
+        ImageProduct4.classList.remove("active-image")
+        ImageProduct2.classList.remove("non-active-image")
+        ImageProduct1.classList.add("non-active-image")
+        ImageProduct3.classList.add("non-active-image")
+        ImageProduct4.classList.add("non-active-image")
+        ImageProductThumbnail2.classList.add('active-item')
+        ImageProductThumbnail1.classList.remove('active-item')
+        ImageProductThumbnail3.classList.remove('active-item')
+        ImageProductThumbnail4.classList.remove('active-item')
+}
+
+function showImageProduct3(){
+    ImageProduct3.classList.add('active-image')
+    ImageProduct2.classList.remove('active-image')
+    ImageProduct1.classList.remove('active-image')
+    ImageProduct4.classList.remove('active-image')
+    ImageProduct3.classList.remove('non-active-image')
+    ImageProduct2.classList.add('non-active-image')
+    ImageProduct1.classList.add('non-active-image')
+    ImageProduct4.classList.add('non-active-image')
+    ImageProductThumbnail3.classList.add('active-item')
+    ImageProductThumbnail2.classList.remove('active-item')
+    ImageProductThumbnail1.classList.remove('active-item')
+    ImageProductThumbnail4.classList.remove('active-item')
+}
+
+function showImageProduct4(){
+    
+    ImageProduct4.classList.add("active-image")
+    ImageProduct2.classList.remove("active-image")
+    ImageProduct3.classList.remove("active-image")
+    ImageProduct1.classList.remove("active-image")
+    ImageProduct4.classList.remove("non-active-image")
+    ImageProduct2.classList.add("non-active-image")
+    ImageProduct3.classList.add("non-active-image")
+    ImageProduct1.classList.add("non-active-image")
+    ImageProductThumbnail4.classList.add('active-item')
+    ImageProductThumbnail2.classList.remove('active-item')
+    ImageProductThumbnail3.classList.remove('active-item')
+    ImageProductThumbnail1.classList.remove('active-item')
+}
+
+
